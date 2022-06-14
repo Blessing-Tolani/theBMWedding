518a9d2c36542e34de9bbce7f4ab69a747252c0c
Blessing-Tolani <ojotolani3@gmail.com>
2022-06-14 11:13:38 +0100

    chore: adjust animation delay

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,7 +74,7 @@ export default function Home() {
         ease: "power2.out",
         y: 0,
         duration: 2,
-        delay: 1,
+        delay: 0.5,
         opacity: 1,
         scrollTrigger: {
           trigger: secondSubText.current,
